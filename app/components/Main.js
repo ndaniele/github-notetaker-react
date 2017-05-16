51976f03ce7c247ff8af3176bd865c04ba8c902c
ndaniele <n.daniele@gmail.com>
2017-05-15 22:26:27 -0400

    created first react component <Main />

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div>
+      Hello World from Nicholas Daniele
+      </div>
+    )
+  }
+});
+
+
+ReactDOM.render(<Main />, document.getElementById('app'))
