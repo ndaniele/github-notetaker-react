005ac9dd81c15a8c36c4d829b27606e5f06e7818
ndaniele <n.daniele@gmail.com>
2017-05-19 11:36:04 -0400

    ES6 refactor of components in Notes folder

--- app/components/Notes/AddNote.js
+++ app/components/Notes/AddNote.js
@@ -1,35 +1,36 @@
-var React = require('react');
-var Notes = require('./Notes');
+import React from 'react';
+import Notes from './Notes';
 
-var AddNote = React.createClass({
-  propTypes: {
-    username: React.PropTypes.string.isRequired,
-    addNote: React.PropTypes.func.isRequired
-  },
-
-  setRef: function(ref){
-    this.note = ref;
-  },
-
-  handleSubmit: function(){
+class AddNote extends React.Component {
+  handleSubmit(){
     var newNote = this.note.value;
     this.note.value = '';
     this.props.addNote(newNote)
-  },
+  }
 
-  render: function(){
+  setRef(ref){
+    this.note = ref;
+  }
+
+
+  render(){
     return (
       <div className="input-group">
-        <input type="text" className="form-control" placeholder="Add New Note" ref={this.setRef}/>
+        <input type="text" className="form-control" placeholder="Add New Note" ref={(ref) => this.setRef(ref)}/>
           <span className="input-group-btn">
-            <button className='btn btn-default' type="button" onClick={this.handleSubmit}>
+            <button className='btn btn-default' type="button" onClick={() => this.handleSubmit()}>
               Submit
             </button>
           </span>
       </div>
     )
   }
-});
 
+};
 
-module.exports = AddNote;
+AddNote.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  addNote: React.PropTypes.func.isRequired
+}
+
+export default AddNote;
--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -1,19 +1,16 @@
-var React = require('react')
+import React from 'react'
 
-var NotesList = React.createClass({
-  PropTypes: {
-    notes: React.PropTypes.array.isRequired
-  },
-  render: function(){
-    var notes = this.props.notes.map(function(note, index){
-      return <li className="list-group-item" key={index}>{note['.value']}</li>
-    })
+class NotesList extends React.Component {
+  render(){
+    const { notes } = this.props;
     return (
       <ul className="list-group">
-        {notes}
+        {notes.map((note, index) => (
+          <li className="list-group-item" key={index}>{note['.value']}</li>
+        ))}
       </ul>
     )
   }
-});
+}
 
-module.exports = NotesList;
+export default NotesList
