9c4dbf8e1d5a58ad9e3bc3fdc4ac9139d0263943
ndaniele <n.daniele@gmail.com>
2017-05-23 12:41:53 -0400

    add process.env.PORT to server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var ecstatic = require('ecstatic');
 
 http.createServer(
   ecstatic({ root: __dirname + '/public' })
-).listen(8080);
+).listen(process.env.PORT || 8080);
 
 console.log('Listening on :8080');
