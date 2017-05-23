e9cdae1f15c0f649c5332cb95b4c13946befe3fb
ndaniele <n.daniele@gmail.com>
2017-05-23 11:11:26 -0400

    refactor server

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var http = require('http');
-var express = require('express');
 var ecstatic = require('ecstatic');
 
-var app = express();
-app.use(ecstatic({ root: __dirname + '/public' }));
-http.createServer(app).listen(8080);
- 
+http.createServer(
+  ecstatic({ root: __dirname + '/public' })
+).listen(8080);
+
 console.log('Listening on :8080');
